1355454b9baab73cf5866f897ecdc8fb02ed115b
ivankazakou34700 <s34700@pjwstk.edu.pl>
2024-10-14 17:01:25 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // TODO: we need to add the missing classes!
 
-// OK, I will add `Adder` and s33309 will add `Subtractor`.
+// OK, I will add `Substractor` and s33309 will add `Adder`.
 
 public class Main {
     public static void main(String[] args) {
